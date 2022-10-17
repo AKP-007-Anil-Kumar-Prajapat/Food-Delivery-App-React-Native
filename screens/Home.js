cce1c2874c1cda8eb64a05fccee656630b5d2159
AKP-007-Anil-Kumar-Prajapat <anil.prajapat@truworth.com>
2022-10-17 18:37:44 +0530

    Running Code

--- screens/Home.js
+++ screens/Home.js
@@ -168,7 +168,7 @@ const Home = ({navigation}) => {
       <TouchableOpacity
         style={{marginBottom: SIZES.padding * 2}}
         onPress={() =>
-          navigation.navigate('Restaurants', {
+          navigation.navigate('Restaurant', {
             item,
             currentLocation,
           })
